662fd578e260d832a4189c0b6e1f3fffbd17b8d9
Morning <morning@MStarss-iMac.local>
2023-03-30 13:06:15 +0800

    day87

---
+++ e-commerce-frontend/src/components/MainMenu.jsx
@@ -0,0 +1,36 @@
+import SubMenu from "./SubMenu";
+import { useState, useEffect } from "react";
+import { Navbar } from "react-bootstrap";
+import { fetchMenu } from "../services/fetchData";
+
+export default function MainMenu() {
+  const URL = "http://localhost:8081/menu/all";
+
+  const [menus, setMenus] = useState([]);
+
+  useEffect(() => {
+    fetchMenu(URL, setMenus);
+  }, []);
+
+  console.log(menus);
+
+  const subMenus = menus.map((subMenu, index) => {
+    return (
+      <SubMenu
+        key={index}
+        id={subMenu._id}
+        title={subMenu.title}
+        position={subMenu.position}
+        children={subMenu.children}
+      />
+    );
+  });
+
+  return (
+    <div>
+      <Navbar bg="light" id="mainMenu-container" className="px-5">
+        {subMenus}
+      </Navbar>
+    </div>
+  );
+}
